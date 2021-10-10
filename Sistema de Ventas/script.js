22943c98bd21f500cbe884dc1779f27f0a0f3f3b
Fengrand <collerx100pre@gmail.com>
2021-10-10 09:02:56 -0500

    Visualizacion de ventas

--- Sistema de Ventas/script.js
+++ Sistema de Ventas/script.js
@@ -1,18 +1,19 @@
 class Busqueda{
     //el constructor nos permitira inicializar variables
     constructor(){
-        this.producto = [
+        this.productos = [
             {ID: "275", nombre: "Tarjeta Grafica", mes: "Enero", ventas:"8500", costos: "4000"},
             {ID: "276", nombre: "Monitor", mes: "Enero", ventas:"4800", costos: "2100"},
             {ID: "277", nombre: "Teclado", mes: "Enero", ventas:"2400", costos: "1500"},
             {ID: "278", nombre: "Mouse", mes: "Enero", ventas:"3000", costos: "1600"},
             {ID: "279", nombre: "Heatset", mes: "Enero", ventas:"2500", costos: "1200"},
             {ID: "280", nombre: "WebCam", mes: "Enero", ventas:"1800", costos: "700"},
+            {ID: "281", nombre: "Microfono", mes: "Enero", ventas:"1500", costos: "600"},     
         ];
         //Backup de la lista para no perderla
-        this.productoBK = this.producto;
+        this.productoBK = this.productos;
         this.onInit();
-        console.log(this.producto);
+        console.log(this.productos);
     }
     //Operaciones de vista
     onInit(){
@@ -23,7 +24,7 @@ class Busqueda{
             cuerpo.deleteRow(0);
         }
         //Insertar los produtos dentro de Tbody
-        this.producto.forEach(producto => {
+        this.productos.forEach(producto => {
             let fila = cuerpo.insertRow(cuerpo.rows.length);
             //Inserta celdas
             fila.insertCell(0).innerHTML = producto.ID;
@@ -32,16 +33,24 @@ class Busqueda{
             fila.insertCell(3).innerHTML = producto.ventas;
             fila.insertCell(4).innerHTML = producto.costos;
         });
+
+        let fila = cuerpo.insertRow(cuerpo.rows.length);
+            fila.insertCell(0).innerHTML = "<b>TOTAL";
+            fila.insertCell(1).innerHTML = "";
+            fila.insertCell(2).innerHTML = "";
+            fila.insertCell(3).innerHTML = this.productos.map(p => parseInt(p.ventas)).reduce((a,b) => a+b);
+            fila.insertCell(4).innerHTML = this.productos.map(p => parseInt(p.costos)).reduce((a,b) => a+b);
+            
     }
     //Metodo de filtrado de datos
     buscar(id){
         //recupero el valor del Input
         let busqueda = document.getElementById(id).value;
         //restauro la lista de personas
-        this.producto = this.productoBK;
+        this.productos = this.productoBK;
         //retorna el resultado de busqueda
-        this.producto = this.producto.filter(producto => {
-            return producto.ID.toLowerCase().indexOf(busqueda) > -1;
+        this.productos = this.productos.filter(producto => {
+            return producto.ID.toLowerCase().indexOf(busqueda) > -1 || producto.nombre.toLowerCase().indexOf(busqueda) > -1;
         });
         //actualizar la vista
         this.onInit();
